8dd5f8f1857e466bbe66ac909b864b1f5c13ec03
CeciliaPerles <cecilia.perles@hotmail.com>
2022-07-28 21:19:59 -0300

    First commit

---
+++ src/com/minhaempresa/PrimeiraClasse.java
@@ -0,0 +1,25 @@
+package com.minhaempresa;
+
+import com.minhaempresa.model.Gato;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+				
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World" + (a + b));*/
+	}
+
+}
+class Livros {
+	private String nome;
+	private String npag;
+	
+}
